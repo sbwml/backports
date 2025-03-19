b9e303fa4f061dd4964464f4f9179f9ab695483d
Felix Fietkau <nbd@nbd.name>
2025-03-19 12:11:45 +0100

    headers: disable EXPORT_SYMBOL_NS_GPL on <6.13
    The macro changed to take a string argument, which cannot be backported easily
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- backport/backport-include/linux/export.h
+++ backport/backport-include/linux/export.h
@@ -5,6 +5,10 @@
 
 #include_next <linux/export.h>
 
+#if LINUX_VERSION_IS_LESS(6,13,0)
+#undef EXPORT_SYMBOL_NS_GPL
+#endif
+
 #ifndef EXPORT_SYMBOL_NS_GPL
 #define EXPORT_SYMBOL_NS_GPL(sym, ns) EXPORT_SYMBOL_GPL(sym)
 #endif
