331feac329fe50e18ec8b5dafddc0f499259dcb2
Felix Fietkau <nbd@nbd.name>
2025-08-04 19:54:22 +0200

    backport hrtimer_setup()
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- backport/backport-include/linux/hrtimer.h
+++ backport/backport-include/linux/hrtimer.h
@@ -10,4 +10,31 @@
 
 #endif /* < 4.16 */
 
+#if LINUX_VERSION_IS_LESS(6,13,0)
+
+static inline enum hrtimer_restart hrtimer_dummy_timeout(struct hrtimer *unused)
+{
+	return HRTIMER_NORESTART;
+}
+
+static inline void
+hrtimer_update_function(struct hrtimer *timer,
+			enum hrtimer_restart (*function)(struct hrtimer *))
+{
+	timer->function = function;
+}
+
+static inline void
+hrtimer_setup(struct hrtimer *timer, enum hrtimer_restart (*function)(struct hrtimer *),
+	      clockid_t clock_id, enum hrtimer_mode mode)
+{
+	hrtimer_init(timer, clock_id, mode);
+	if (WARN_ON_ONCE(!function))
+		timer->function = hrtimer_dummy_timeout;
+	else
+		timer->function = function;
+}
+
+#endif
+
 #endif /* __BACKPORT_LINUX_HRTIMER_H */
