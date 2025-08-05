6194bf852a3edef3a5f953c4b282cda420fb83db
Felix Fietkau <nbd@nbd.name>
2025-08-05 09:59:44 +0200

    backport roundup_u64()
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- backport/backport-include/linux/math64.h
+++ backport/backport-include/linux/math64.h
@@ -8,4 +8,14 @@
 extern u64 mul_u64_u64_div_u64(u64 a, u64 b, u64 c);
 #endif /* < 5.9 */
 
+#ifndef DIV_U64_ROUND_UP
+#define DIV_U64_ROUND_UP(ll, d) \
+	({ u32 _tmp = (d); div_u64((ll) + _tmp - 1, _tmp); })
+
+static inline u64 roundup_u64(u64 x, u32 y)
+{
+	return DIV_U64_ROUND_UP(x, y) * y;
+}
+#endif /* DIV_U64_ROUND_UP */
+
 #endif /* __BACKPORT_LINUX_MATH64_H */
